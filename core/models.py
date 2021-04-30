e1be23f43e62a2ec50a7abbd42a041c0c9813141
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2021-04-30 09:01:55 +0100

    added the templatetags for resume

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
-class Skills(models.Model):
+class Skill(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
 
@@ -25,9 +27,16 @@ class WorkExperience(models.Model):
     description = models.TextField()
     currently_working_here = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = "start_date",\
+
     def __str__(self):
         return f"{self.position} at {self.company}"
 
-    
-    class Meta:
-        ordering = "start_date",
+    def save(self, *args, **kwargs):
+        if self.end_date and self.currently_working_here:
+            raise ValidationError(_("You can either set the End date or Currently working here but not both."))
+        if not self.end_date and not self.currently_working_here:
+            raise ValidationError(_("You must set the date you stopped working here"))
+        return super(WorkExperience, self).save(*args, **kwargs)
+
--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,3 +1,21 @@
 from django import template
+from ..models import Skill, WorkExperience, TechStack
+
 
 register = template.Library()
+
+
+@register.inclusion_tag('core/dummy.html', takes_context=True)
+def list_skills(context, template="core/skills.html"):
+	
+	skills = Skill.objects.all()[:5]
+	
+	return {'skills': skills, 'template': template}
+
+
+@register.inclusion_tag('core/dummy.html', takes_context=True)
+def list_work_experiences(context, template="core/experience.html"):
+
+	experiences = WorkExperience.objects.all()[:5]
+	
+	return {'experiences': experiences, 'template': template}

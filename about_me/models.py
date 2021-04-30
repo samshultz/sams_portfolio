b5110096342a7b818d817d8f362ae4e8388c3a45
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2021-04-30 06:06:33 +0100

    initial commit

---
+++ about_me/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+
+class AboutMe(models.Model):
+    AVAILABLE = 'available'
+    NOT_AVAILABLE = 'not available'
+
+    FREELANCE_CHOICES = (
+        (AVAILABLE, 'Available'),
+        (NOT_AVAILABLE, 'Not Available')
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    description = models.TextField()
+    phone = models.CharField(_("Phone Number"), max_length=50)
+    freelance = models.CharField(choices=FREELANCE_CHOICES, default=AVAILABLE, max_length=13)
+
+    
+    class Meta:
+        """Meta definition for AboutMe."""
+
+        verbose_name = 'About Me'
+        verbose_name_plural = 'About Me'
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+    def save(self):
+        if not self.pk and AboutMe.objects.exists():
+            raise ValidationError(_("There can only be one about me."))
+        return super(AboutMe, self).save(*args, **kwargs)
+
+
+    @property
+    def email(self):
+        return self.user.email
+    
+    @property
+    def fullname(self):
+        return self.user.get_full_name()
+

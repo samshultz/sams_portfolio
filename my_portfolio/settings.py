b4bd69ebf9bb64706ec0b0620d7efbf24bab3e76
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2021-05-07 12:33:58 +0100

    change S3 bucket name

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -162,17 +162,19 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'samshultz-personal-assets'
+AWS_STORAGE_BUCKET_NAME = 'samshultz-portfolio-assets'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_DEFAULT_ACL = None
+AWS_LOCATION = 'static'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static'
-
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'my_portfolio.storage_backends.StaticS3Storage'
+
 DEFAULT_FILE_STORAGE = 'my_portfolio.storage_backends.MediaStorage'
 
 # Default primary key field type
--- my_portfolio/storage_backends.py
+++ my_portfolio/storage_backends.py
@@ -1,5 +1,12 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
+
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
+
+
+class StaticS3Storage(S3Boto3Storage):
+   location = settings.AWS_LOCATION
+   default_acl = 'public-read'
